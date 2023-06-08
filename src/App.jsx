11826eb8baf6e526f88daa031c8a0a1f7e4a95cd
Max Abimbola <max.abimbola@hotmail.com>
2023-06-08 16:22:37 +0100

    feat: correctly import wordHunterDisplay component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import WordHunterDisplay from './wordHunterDisplay.jsx'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>You're here</div>
+    <WordHunterDisplay />
+
   )
 }
 
---
+++ src/wordHunterDisplay.jsx
@@ -0,0 +1,10 @@
+import { useState} from "react";
+import React from 'react'
+
+function WordHunterDisplay(props){
+    return (
+        <div>dfsgfd</div>
+    )
+}
+
+export default WordHunterDisplay;
