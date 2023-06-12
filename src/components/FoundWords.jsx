cfebfe8960069b06c7e1f3d821ce2f1f9c9e9926
Max Abimbola <max.abimbola@hotmail.com>
2023-06-12 18:08:39 +0100

    feat: modify foundwords to output found words one by one

--- src/components/FoundWords.jsx
+++ src/components/FoundWords.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { useState} from "react";
-import '../FoundWords.css'
-function FoundWords(props){
-    return(
-            <textarea readonly='yes' value={props.words} className='word-list'>
-            </textarea>
-    )
+import React from 'react';
+import '../FoundWords.css';
+
+function FoundWords(props) {
+  let words = [];
+  
+  if (Array.isArray(props.words)) {
+    words = props.words;
+  }
+
+  return (
+    <textarea readOnly value={words.join('\n')} className='word-list'></textarea>
+  );
 }
 
-export default FoundWords
+export default FoundWords;
